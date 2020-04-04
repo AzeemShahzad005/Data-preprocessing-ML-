0428284299977fb52b8d6fbf423010a547c5a478
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2020-04-04 12:46:10 +0500

    Update Data-preprocessing(ML).py

--- Data-preprocessing(ML).py
+++ Data-preprocessing(ML).py
@@ -15,7 +15,7 @@ dataset = pd.read_csv('Data.csv')
  X = dataset.iloc[: , :-1].values
  Y = dataset.iloc[: , 3].values
  
- #
+ #Missing Data
  from sklearn.preprocessing import Imputer 
  imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis= 0)
  imputer = imputer.fit(X[: , 1:3])
