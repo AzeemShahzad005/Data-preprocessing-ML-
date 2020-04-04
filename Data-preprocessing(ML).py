ea8dd84bb5fd5f4ddcb1420685b2e4b6e18c0984
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2020-04-04 12:45:48 +0500

    Update Data-preprocessing(ML).py

--- Data-preprocessing(ML).py
+++ Data-preprocessing(ML).py
@@ -5,16 +5,20 @@ Created on Wed Apr  1 12:55:45 2020
 @author: Azeem
 """
 
+#Importing libraries   
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Importing Dataset
 dataset = pd.read_csv('Data.csv')
  X = dataset.iloc[: , :-1].values
  Y = dataset.iloc[: , 3].values
  
+ #
  from sklearn.preprocessing import Imputer 
  imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis= 0)
  imputer = imputer.fit(X[: , 1:3])
  X[: , 1:3] = imputer.transform(X[: , 1:3])
+ 
  
