0c09b861b8fbbcb81aa680e8cef686deecc00f0a
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2020-04-19 12:27:01 +0500

    Update Data-preprocessing(ML).py

--- Data-preprocessing(ML).py
+++ Data-preprocessing(ML).py
@@ -32,4 +32,9 @@ dataset = pd.read_csv('Data.csv')
  from sklearn.cross_validation import train_test_split
  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
  
+ # Feature Scaling
+ from sklearn.preprocessing import StandardScaler
+ sc_X = StandardScaler()
+ X_train = sc_X.fit_transform(X_train)
+ X_text = sc_X.transform(X_test)
  
