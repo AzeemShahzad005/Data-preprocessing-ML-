b4239649aed207ce109dbe6dd445e704f702f733
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2020-04-19 12:25:47 +0500

    Update Data-preprocessing(ML).py

--- Data-preprocessing(ML).py
+++ Data-preprocessing(ML).py
@@ -28,4 +28,8 @@ dataset = pd.read_csv('Data.csv')
  labelencoder_y = LabelEncoder()
  y = labelencoder_y.fit_transform(y)
  
+ # Splitting the dataset into the Training set and Test set
+ from sklearn.cross_validation import train_test_split
+ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+ 
  
